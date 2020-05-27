60641d6ac004cdee70865c112b20473a7d15a3ed
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2020-05-27 13:44:30 +0200

    Implement simple jinja2 parser

---
+++ main.py
@@ -0,0 +1,29 @@
+from jinja2 import Template
+
+todos = [
+    "Geschenk 1",
+    "Geschenk 2",
+    "Geschenk 3"
+]
+
+# read template
+wish_handle = open("templates/wishlist.html", "r")
+template_text = wish_handle.read()
+wish_handle.close()
+
+# render template
+template = Template(template_text)
+wishprint = template.render(wishlist = todos)
+
+# save output
+print(wishprint)
+output_handle = open("output/wishlist.html", "w")
+output_handle.write(wishprint)
+output_handle.close()
+
+
+# handle = open("templates/test.txt", "r")
+# template = Template(handle.read())
+# printable = template.render(name="TheCaptain")
+# print(printable)
+# handle.close()
